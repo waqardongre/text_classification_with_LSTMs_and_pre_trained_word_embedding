c455b4da98a4607bedac27e681e35672fe9aa457
waqardongr <waqar@DESKTOP-PHRTA1N.localdomain>
2022-02-16 12:05:09 +0530

    minor changes in app.py

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def take_inp():
         <body>
         <form method="post" class="form">
         <label>Enter your review:</label>
-        <textarea name="text" rows="2" cols="50"> awesome movie </textarea>
+        <textarea name="text" rows="2" cols="50">I'm from the uk so we don't really do all that kind of cheering in cinemas. But oh my god. I've never seen anything like it there were so many moments during this film where the cinema just exploded, pure excitement coming from almost everyone, including me.
+
+Even though it was quite bittersweet i cannot wait to watch this movie again, this is a top 3 mcu movie.
+
+The score was just fantastic, acting amazing, even the small bits of fan service just put a smile on my face. There's not much else i can say about this film, except how excited it's made me for the future of the mcu.
+
+</textarea>
         <input type="submit" value="Predict"/>
         </form>
         <p>Sample inputs:</p>
@@ -47,15 +53,15 @@ def predict(text:str = Form(...)):
     vectorizer.set_weights(from_disk['weights'])
     
     x = vectorizer(np.array([[s] for s in [text.replace(" ", "")]])).numpy()
-
+    print(x)
     pred = loaded_model.predict(x)[0][0]
 
     class_names = ['Positive', 'Negative']
-    t_sentiment = class_names[int(0.5 < pred)]
+    t_sentiment = class_names[int(pred < 0.5)]
 
-    return { #return the dictionary for endpoint
-        "your_review": text,
-        "predicted_sentiment": t_sentiment,
-        "probabs_closer_to_0_positive_closer_to_1_negative": pred
-    }
-    #return '''Your input: \''''+ str(text) + '''\' and your review is predicted as: ''' + str(t_sentiment)
+    # return { #return the dictionary for endpoint
+    #     "your_review": text,
+    #     "predicted_sentiment": t_sentiment,
+    #     "probabs_closer_to_0_positive_closer_to_1_negative": pred
+    # }
+    return '''Your input: \''''+ str(text) + '''\' and your review is predicted as: ''' + str(t_sentiment) + ''' with probability ''' + str(pred)
